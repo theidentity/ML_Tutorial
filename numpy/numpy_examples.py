2e1f7f48dc356d95ac8a5ccfdade60bf1fb7fd58
balu <balgopal.u@gmail.com>
2017-12-17 01:31:46 +0530

    Added numpy examples

---
+++ numpy/numpy_examples.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+a = np.array([[2,3],
+	[4,5],
+	[6,7],
+	[8,9]])
+b = np.array([1,2])
+
+# print(a)
+# print(a.shape)
+
+# print(b)
+# print(b.shape)
+
+# print(a**2)
+# print(np.mean(a))
+# print(np.mean(a[:,0]))
+# print(np.mean(a[:,1]))
+
+# print(np.sum(a))
+# print(np.sum(a,axis=0))
+# print(np.sum(a,axis=1))
+
+# print(a+b)
+# print(b-a)
+# print(np.absolute(b-a))
+
+
